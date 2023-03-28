f3e66409a45808fcb42f67510694b908d1995f94
luiz neto <luiz.neto@sigep.com.br>
2023-03-28 14:42:13 -0300

    Comecando a paginda de registro de usuario no js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { Card } from "./componetes/Card/LeftCard";
+import { RigthCard } from "./componetes/Card/RigthCard";
 import { Estilos } from "./componetes/EstilosGloais/Estilos";
 
 function App() {
   return (
     <div>
       <Estilos/>
-      <h1>Registro de Novos Usuarios</h1>
+      <RigthCard>
+      </RigthCard>
+      <Card>
+        <h1>Registro de Novos Usuarios</h1>
+      </Card>
+      
     </div>
   );
 }
